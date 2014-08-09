c25d5f0f15f8228c971a46e56d6a9f1c669d58c9
Christopher Roy Bratusek <nano@jpberlin.de>
2014-08-09 19:09:28 +0200

    update german translation

--- translations/phototonic_de.ts
+++ translations/phototonic_de.ts
@@ -11,19 +11,7 @@
     <message>
         <location filename="../dialogs.cpp" line="1184"/>
         <source>Name</source>
-        <translation type="unfinished">Name</translation>
-    </message>
-    <message>
-        <source>Command</source>
-        <translation type="vanished">Befehl</translation>
-    </message>
-    <message>
-        <source>Path</source>
-        <translation type="vanished">Pfad</translation>
-    </message>
-    <message>
-        <source>Add</source>
-        <translation type="vanished">Hinzufügen</translation>
+        <translation>Name</translation>
     </message>
     <message>
         <location filename="../dialogs.cpp" line="1186"/>
@@ -33,12 +21,12 @@
     <message>
         <location filename="../dialogs.cpp" line="1192"/>
         <source>Choose</source>
-        <translation type="unfinished"></translation>
+        <translation>Auswählen</translation>
     </message>
     <message>
         <location filename="../dialogs.cpp" line="1196"/>
         <source>Add manually</source>
-        <translation type="unfinished"></translation>
+        <translation>Manuell hinzufügen</translation>
     </message>
     <message>
         <location filename="../dialogs.cpp" line="1200"/>
@@ -58,15 +46,7 @@
     <message>
         <location filename="../dialogs.cpp" line="1259"/>
         <source>New Application</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <source>Error</source>
-        <translation type="obsolete">Fehler</translation>
-    </message>
-    <message>
-        <source>Not an executable</source>
-        <translation type="obsolete">Keine ausführbare Datei</translation>
+        <translation>Neue Applikationi</translation>
     </message>
 </context>
 <context>
@@ -608,12 +588,12 @@
     <message>
         <location filename="../mainwindow.cpp" line="820"/>
         <source>&lt;u&gt;Contributors&lt;/u&gt;:&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;u&gt;Mitwirkende&lt;/u&gt;:&lt;br&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="822"/>
         <source>&lt;u&gt;Translators&lt;/u&gt;:&lt;br&gt;</source>
-        <translation type="unfinished"></translation>
+	<translation>&lt;u&gt;Übersetzer&lt;/u&gt;:&lt;br&gt;</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="2039"/>
@@ -882,10 +862,6 @@
         <translation>&lt;p&gt;Kompiliert mit Qt</translation>
     </message>
     <message>
-        <source>Translators:&lt;br&gt;</source>
-        <translation type="vanished">Übersetzer:&lt;br&gt;</translation>
-    </message>
-    <message>
         <location filename="../mainwindow.cpp" line="857"/>
         <location filename="../mainwindow.cpp" line="2394"/>
         <source>Invalid selection</source>
@@ -1008,10 +984,6 @@
         <location filename="../mainwindow.cpp" line="2014"/>
         <source>Slide show stopped</source>
         <translation>Diashow angehalten</translation>
-    </message>
-    <message>
-        <source>End Slide Show</source>
-        <translation type="obsolete">Diashow beenden</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="2040"/>
@@ -1142,14 +1114,6 @@
 </context>
 <context>
     <name>ResizeDialog</name>
-    <message>
-        <source>Resize Image (Demo)</source>
-        <translation type="obsolete">Bildgröße ändern (Demo)</translation>
-    </message>
-    <message>
-        <source>OK</source>
-        <translation type="obsolete">OK</translation>
-    </message>
     <message>
         <location filename="../dialogs.cpp" line="759"/>
         <source>Scale Image</source>
@@ -1373,10 +1337,6 @@
         <location filename="../dialogs.cpp" line="499"/>
         <source>Keyboard Shortcuts:</source>
         <translation>Tastenkombinationen:</translation>
-    </message>
-    <message>
-        <source>keyboard shortcuts:</source>
-        <translation type="obsolete">Tastenkombinationen:</translation>
     </message>
     <message>
         <location filename="../dialogs.cpp" line="510"/>
