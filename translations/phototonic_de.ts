d77589838712d812e2aa1bff4d20a00031e78ad6
Jonathan Hooverman <Jonathan.Hooverman@gmail.com>
2015-02-02 18:52:46 +0100

    Updated German translation

--- translations/phototonic_de.ts
+++ translations/phototonic_de.ts
@@ -891,7 +891,7 @@
     <message>
         <location filename="../mainwindow.cpp" line="451"/>
         <source>Up</source>
-        <translation>Nach oben</translation>
+        <translation>Übergeordnetes Verzeichnis</translation>
     </message>
     <message>
         <location filename="../mainwindow.cpp" line="455"/>
@@ -2000,14 +2000,14 @@
     <message>
         <location filename="../thumbview.cpp" line="420"/>
         <source>Selected %1 of%2</source>
-        <translation type="unfinished"></translation>
+        <translation>Ausgewählt %1 von %2</translation>
     </message>
     <message numerus="yes">
         <location filename="../thumbview.cpp" line="422"/>
         <source> %n image(s)</source>
-        <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+        <translation>
+            <numerusform> %n Bild</numerusform>
+            <numerusform> %n Bilder</numerusform>
         </translation>
     </message>
     <message>
